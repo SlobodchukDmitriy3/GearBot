f622a21c64675ca4bca6320158280cc09ee1e060
Dmitriy Slobodchuk <104769679+SlobodchukDmitriy3@users.noreply.github.com>
2023-03-14 13:53:25 +0200

    Add files via upload

---
+++ config.py.py
@@ -0,0 +1,16 @@
+#token
+TOKEN = 'here - Token'
+#id of the bot
+POST_ID = 'id'
+ROLES = {
+  'id role1', 
+  'id role2', 
+  'id role3',
+  'id role4', 
+  'id role5', 
+  'id role6', 
+}
+#roles on server 
+EXCLORES = ()
+
+MAX_ROLES_PER_USER = 3 #Max roles that user can choose 
